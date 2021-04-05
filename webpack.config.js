33c2ee449efd428a0d506e0170b4b8c9ad2d3531
Devon Poston <dev.poston@gmail.com>
2021-04-04 23:19:52 -0700

    debugging webpack error - cannot resolve

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,70 @@
+// let path = require('path');
+
+// module.exports = {
+//   entry: './client/src/index.jsx',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.join(__dirname, '/dist')
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015']
+//         }
+//       },
+//       {
+//         test: /\.css$/i,
+//         loader: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.(jpe?g|png|gif|svg)$/i,
+//         loader: 'file-loader',
+//         options: {
+//           name: '/public/icons/[name].[ext]'
+//         }
+//       }
+//     ]
+//   }
+// };
+
 const path = require('path');
+const src_dir = path.join(__dirname, '/client/src');
+const dist_dir = path.join(__dirname, '/client/dist');
 
 module.exports = {
-  entry: './client/src/index.jsx',
+  entry: [`${src_dir}/index.jsx`],
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, '/dist')
+    path: dist_dir
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        use: ['babel-loader'],
-        query: {
-          presets: ['react', 'es2015']
-        }
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       },
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        include: src_dir,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          }
+        ]
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
   }
 };
